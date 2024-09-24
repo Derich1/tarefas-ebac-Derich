32b3b3dad207e34f19a81521921ecbb3ce4d46cf
Derich <derich.rosario22@gmail.com>
2024-09-24 16:51:01 -0300

    enviando tarefa do modulo 13

---
+++ mod13/Tarefa_mod13/src/br/com/derich/PessoaFisica.java
@@ -0,0 +1,16 @@
+package br.com.derich;
+
+public class PessoaFisica extends Pessoa {
+
+    private Long cpf;
+
+    public Long getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(Long cpf) {
+        this.cpf = cpf;
+    }
+
+
+}
---
+++ mod13/Tarefa_mod13/src/br/com/derich/PessoaJuridica.java
@@ -0,0 +1,14 @@
+package br.com.derich;
+
+public class PessoaJuridica {
+
+    private Long cnpj;
+
+    public Long getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(Long cnpj) {
+        this.cnpj = cnpj;
+    }
+}
